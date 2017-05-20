208165f018aa88152f77f6f115f535e35714e6a9
maxeskell <maxeskell1@gmail.com>
2017-05-20 16:16:55 +0100

    first working commit -mvp

--- src/js/app.js
+++ src/js/app.js
@@ -5,50 +5,73 @@ $(() => {
   const $badgearea = $('.badgearea');
   const $squares = $('.square');
   const $button = $('.button');
-  const $scoreScreen = $('.scoresScreen');
-  const $timerScreen = $('.timerScreen');
-  const $highscoreScreen = $('.highscoreScreen');
+
+  //global vaiables for user feedback
+  const $timerScreen = $('#timerScreen');
+  const $clickScoreScreen = $('#clickScoreScreen');
+  const $userScoreScreen = $('#userScoreScreen');
+  const $fastestClickSreen = $('#fastestClickSreen');
+  const $highscoreScreen = $('#highscoreScreen');
+  const $averageCLickScreen = $('#highscoreAverageCLickScreen');
+  const $highscoreFastestClickScreen = $('.highscoreFastestClickScreen');
 
   // timer constants
   let timeRemaining = 20;
   let timerId = null;
+  let delay = 200;
 
   // game logic constants
   let gameOn = false;
-  var score = 0;
+
+  //game constants
+  let userScore = 0;
+  let clickScore = 0;
+  let otterScore = 0;
+  let beaverScore = 0;
+  let blankScore = 0;
+  let userFastestClickTime = 0;
+  let userAverageClickTime = 0;
+
+  //high score constants
   let highscore = 0;
-  let delay = 200;
+  let highscoreFastestClickTime = 0;
+  let highscoreAverageClickTime = 0;
+
+
 
   //start game when 'start button' is pushed, only if game not started
   $button.on('click', function() {
     if (gameOn === false || timeRemaining === 0) {
-      console.log('start button clicked');
       //store that game has been started
       gameOn = true;
       //update highscore and highscore screen
-      if (score > highscore) {
-        highscore = score;
-        $highscoreScreen.text(highscore);
+      if (userScore > highscore) {
+        highscore = userScore;
+        $highscoreScreen.html(highscore);
       }
-      //reset score
-      score = 0;
+      //reset score and click score
+      userScore = 0;
+      clickScore = 0;
       //rest timer and update timer screen
       clearInterval(timerId);
       timeRemaining = 20;
+      //run function to show beavers and otters for time interval
+      showAnimals();
       //start timer
       timerId = setInterval(() => {
         timeRemaining--;
-        $timerScreen.text(timeRemaining);
-
+        $timerScreen.html(timeRemaining);
         if (timeRemaining === 0) {
           clearInterval(timerId);
           gameOn = false;
+          //empty board
         }
       }, 1000);
-      //run function to show beavers
-      addBeaver();
+
     }
   });
+
+  //reset function
 
   //select random square
   const selectRandomSquare = function() {
@@ -56,40 +79,65 @@ $(() => {
     return $squares.eq(randomIndex);
   };
 
-  //add beaver class to random square, then call function again at an random interval (between 3 and 1 second)
-  const addBeaver = function() {
+// randomise which animal to call (currenlty otter or beaver)
+const randomAnimal = function() {
+  return ( Math.random() * (100-1)+1 ) < 30 ? 'otter' : 'beaver';
+};
+
+  //add beaver/otter class to random square, then call function again at an random interval (between 3 and 1 second)
+  const showAnimals = function() {
     setTimeout(() => {
-      selectRandomSquare().addClass('beaver');
+      selectRandomSquare().addTemporaryClass(randomAnimal(), 1000);
       delay = Math.random() * 1000 * (3 - 1) + 1;
-      addBeaver();
+      showAnimals();
     }, delay);
   };
 
-
   //function to show otters
 
-  //game logic
-
-
+  //game logic for user clicking on beavers/otters/empty square
   $gameholder.on('click', function(e) {
     if (gameOn === true) {
       if ($(e.target).hasClass('beaver')) {
-        score += 20;
+        clickScore = 20;
+        userScore += 20;
+        beaverScore += 1;
         ($(e.target).removeClass('beaver'));
         //make sound
       } else if ($(e.target).hasClass('otter')) {
-        score -= 20;
+        clickScore = -10;
+        userScore -= 20;
+        otterScore += 1;
         ($(e.target).removeClass('otter'));
         //make sound
       } else {
-        score -= 5;
+        clickScore = -5;
+        userScore -= 5;
+        blankScore += 1;
         //make sound
       }
-      console.log('game area clicked');
-      console.log(`score = ${score}`);
-      $scoreScreen.text(score);
+      $clickScoreScreen.text(clickScore);
+      $userScoreScreen.text(userScore);
     }
   });
 
+  //create 'addTemporaryClass' function
+  (function($) {
+
+    $.fn.extend({
+
+      addTemporaryClass: function(className, duration) {
+        var elements = this;
+        setTimeout(function() {
+          elements.removeClass(className);
+        }, duration);
+
+        return this.each(function() {
+          $(this).addClass(className);
+        });
+      }
+    });
+
+  })(jQuery);
 
 });
